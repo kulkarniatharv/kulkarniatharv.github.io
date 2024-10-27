d2eb2f23391cedf7b3734c8a3ef43e7326ca45bf
kulkarniatharv <kulkarniatharv@users.noreply.github.com>
2024-10-27 15:14:02 +0000

    deploy: 71d0b3121fc956c9a5909b29e744ecb56a0ffc8d

---
+++ 252f366e-babe4eeb9c7a4268dd85.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkatharv_kulkarni=self.webpackChunkatharv_kulkarni||[]).push([[916],{9644:function(t,a,n){n.d(a,{S3Y:function(){return h}});var r=n(4084);function h(t){return(0,r.k5)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{fill:"none",d:"M0 0h24v24H0z"}},{tag:"path",attr:{d:"M19 9h-4V3H9v6H5l7 7 7-7zM5 18v2h14v-2H5z"}}]})(t)}}}]);
+//# sourceMappingURL=252f366e-babe4eeb9c7a4268dd85.js.map
